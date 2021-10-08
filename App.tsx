01ab9d1a23042688d3bbb70eca779cad2abbe48e
kevinliao <q85292542000@gmail.com>
2021-10-08 20:04:41 +0800

    Remove the unused styles

--- App.tsx
+++ App.tsx
@@ -57,12 +57,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
