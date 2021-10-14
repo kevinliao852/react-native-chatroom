b88f7972a8f26e0d9bbdc1cd7a14a8e3b72236f6
kevinliao <q85292542000@gmail.com>
2021-10-14 18:26:15 +0800

    Fix the problem that login page didn't detect if a user has been log in or not

--- Screens/Login.tsx
+++ Screens/Login.tsx
@@ -9,38 +9,52 @@ export const Login = () => {
   const navigation = useNavigation<any>();
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [Loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
     (async () => {
-      if (firebase.auth().currentUser) {
-        navigation.navigate('ChatList');
-      }
+      const subscriber = firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          navigation.navigate('ChatList');
+        } else {
+          setLoading(false);
+        }
+      });
+      return subscriber; // unsubscribe on unmount
     })();
   });
 
   return (
     <View style={{ padding: 50, paddingTop: 100 }}>
-      <Input
-        label="Username"
-        onChangeText={(value: string) => setUsername(value)}
-      />
-      <Input
-        label="Password"
-        onChangeText={(value: string) => setPassword(value)}
-        secureTextEntry
-      />
-      <Button
-        onPress={() => {
-          (async () => {
-            console.log(username, password);
-            await firebase
-              .auth()
-              .signInWithEmailAndPassword(username, password)
-              .then(() => navigation.navigate('ChatList'))
-              .catch(console.log);
-          })();
-        }}
-        title="Login"
-      />
+      {Loading ? (
+        <View>
+          <Text style={{ textAlign: 'center' }}>Loading</Text>
+        </View>
+      ) : (
+        <View>
+          <Input
+            label="Username"
+            onChangeText={(value: string) => setUsername(value)}
+          />
+          <Input
+            label="Password"
+            onChangeText={(value: string) => setPassword(value)}
+            secureTextEntry
+          />
+          <Button
+            onPress={() => {
+              (async () => {
+                console.log(username, password);
+                await firebase
+                  .auth()
+                  .signInWithEmailAndPassword(username, password)
+                  .then(() => navigation.navigate('ChatList'))
+                  .catch(console.log);
+              })();
+            }}
+            title="Login"
+          />
+        </View>
+      )}
     </View>
   );
 };
