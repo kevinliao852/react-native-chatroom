e9e57997e6138e268ab1104999f310d4477368ea
kevinliao <q85292542000@gmail.com>
2021-12-03 19:32:45 +0800

    feat: refactor the code and add a logic for user to tap the friend icon and navigate to the chatroom.

--- Screens/FriendList.tsx
+++ Screens/FriendList.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/core";
 import firebase from "firebase";
 import React, { useContext, useEffect, useState } from "react";
 import { ScrollView, TouchableOpacity, View } from "react-native";
@@ -5,45 +6,91 @@ import { Text, Image } from "react-native-elements";
 import { UserContext } from "../Context/UserContext";
 import { Nav } from "./components/Nav";
 
-const RenderFriends = (friend: any, index: any, user: any) => {
+const chatRoom = "ChatRoom2";
+const roomsCollectionName = "Rooms";
+const roomUserRelations = "User_ChatRoom";
+
+//TODO: extract the fetch logic to apply repository pattern
+const createRoomInChatRoom = () =>
+  firebase
+    .firestore()
+    .collection(chatRoom)
+    .doc()
+    .collection(roomsCollectionName)
+    .doc();
+
+const getMyChatListByUserId = (userId: string) =>
+  firebase
+    .firestore()
+    .collection(roomUserRelations)
+    .where("userId", "==", userId)
+    .get();
+
+const isTheFriendInMyChatList = ({ chatList, friend }: any) => {
+  let rooms: Array<any> | undefined;
+  chatList.forEach((documentSnapshot: any) => {
+    rooms = documentSnapshot.data().rooms as Array<any>;
+  });
+
+  if (!Array.isArray(rooms)) {
+    throw new Error("rooms is not an array");
+  }
+
+  return rooms.filter((room: any) => room.friendId === friend.id).length > 0;
+};
+
+const getFriendRoom = ({ chatList, friend }: any) => {
+  let rooms: Array<any> | undefined;
+  chatList.forEach((documentSnapshot: any) => {
+    rooms = documentSnapshot.data().rooms as Array<any>;
+  });
+
+  if (!Array.isArray(rooms)) {
+    throw new Error("rooms is not an array");
+  }
+
+  const filteredRoom = rooms.filter((room: any) => room.friendId === friend.id);
+
+  if (filteredRoom.length !== 1) {
+    console.log(filteredRoom);
+    throw new Error("there are two chatRooms to ur friend ");
+  }
+
+  return filteredRoom[0];
+};
+
+const getChatListId = (chatList: any) => {
+  let id: string | undefined;
+  chatList.forEach((documentSnapshot: any) => {
+    id = documentSnapshot.id;
+  });
+
+  if (id === undefined) {
+    throw new Error("chatLIst id is not found");
+  }
+  return id;
+};
+
+const renderFriends = (friend: any, index: any, user: any) => {
+  const navigation = useNavigation<any>();
+
   return (
     <TouchableOpacity
       key={index}
       onPress={async () => {
-        console.log(friend);
+        const rooms: Array<any> = [];
         const batch = firebase.firestore().batch();
-        let rooms: Array<any> = [];
+        const newChatRoom = createRoomInChatRoom();
+        const chatList = await getMyChatListByUserId(user.uid);
+        const chatListId = getChatListId(chatList);
 
-        const newChatRoom = firebase
-          .firestore()
-          .collection("ChatRoom2")
-          .doc()
-          .collection("Room")
-          .doc();
-
-        let hasRoom = false;
-
-        const chatListId = await firebase
-          .firestore()
-          .collection("User_ChatRoom")
-          .where("userId", "==", user.uid)
-          .get()
-          .then((querySnapshot: any) => {
-            let id;
-            querySnapshot.forEach((documentSnapshot: any) => {
-              rooms = documentSnapshot.data().rooms;
-              id = documentSnapshot.id;
-              hasRoom =
-                rooms.filter((item: any) => item.friendId === friend.id)
-                  .length > 0;
-            });
-            return id;
-          });
-
-        if (hasRoom) {
+        if (isTheFriendInMyChatList({ chatList, friend })) {
+          const room = getFriendRoom({ chatList, friend });
+          navigation.navigate("ChatRoom", { roomId: room.roomId });
           return;
         }
 
+        //TODO: change the default rootName
         rooms.push({
           roomId: newChatRoom.id,
           roomName: newChatRoom.id,
@@ -56,7 +103,6 @@ const RenderFriends = (friend: any, index: any, user: any) => {
           .doc(chatListId);
 
         batch.set(newChatRoom, {});
-        console.log(rooms, friend);
         batch.update(ChatList, { rooms });
         await batch.commit();
       }}
@@ -100,7 +146,7 @@ export const FriendList = () => {
             .data()
             .friendIds.map((item: any) => item.get());
         });
-        console.log("data", data);
+        //console.log("data", data);
         return Promise.all(data);
       })
       .then((res) =>
@@ -109,7 +155,7 @@ export const FriendList = () => {
         })
       )
       .then((res) => {
-        console.log(res);
+        //console.log(res);
         return res;
       })
       .then(setFriends)
@@ -120,9 +166,11 @@ export const FriendList = () => {
     <View style={{ flex: 1, backgroundColor: "black" }}>
       <ScrollView style={{ flex: 1, backgroundColor: "white" }}>
         {friends &&
-          friends.map((friend: any, index: any) =>
-            RenderFriends(friend, index, user)
-          )}
+          friends
+            .filter((friend: any) => friend.id !== user.uid)
+            .map((friend: any, index: any) =>
+              renderFriends(friend, index, user)
+            )}
       </ScrollView>
       <Nav />
     </View>
