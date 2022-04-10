505118064a92601dbb3c39ec581bf5a6275e6fc5
kevinliao <q85292542000@gmail.com>
2022-04-11 00:15:12 +0800

    feat: user can add friends in the friend list.

--- Screens/components/AddFriendOverlay.tsx
+++ Screens/components/AddFriendOverlay.tsx
@@ -1,14 +1,41 @@
-import React, { forwardRef, useImperativeHandle, useState } from "react";
-import { TextInput, View } from "react-native";
+import firebase from "firebase";
+import React, {
+  forwardRef,
+  useContext,
+  useImperativeHandle,
+  useState,
+} from "react";
+import { TextInput, TouchableOpacity, View } from "react-native";
 import { Button, Overlay, Text } from "react-native-elements";
+import { UserContext } from "../../Context/UserContext";
 
 export const AddFriendOverlay = forwardRef((prop: any, ref: any) => {
   const [visible, setVisible] = useState(false);
+  const { user } = useContext<any>(UserContext);
+  const [friendId, setFriendId] = useState("");
   useImperativeHandle(ref, () => ({ toggleOverlay }));
+
+  console.log(friendId);
 
   const toggleOverlay = () => {
     setVisible(!visible);
   };
+
+  function addFriend(friendRef: firebase.firestore.DocumentReference) {
+    return firebase
+      .firestore()
+      .collection("Friendship")
+      .where("userId", "==", user.uid)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((documentSnapshot) => {
+          const friendship = documentSnapshot.ref;
+          friendship.update({
+            friendIds: firebase.firestore.FieldValue.arrayUnion(friendRef),
+          });
+        });
+      });
+  }
 
   return (
     <View>
@@ -17,12 +44,39 @@ export const AddFriendOverlay = forwardRef((prop: any, ref: any) => {
         onBackdropPress={toggleOverlay}
         overlayStyle={{
           padding: 40,
+          width: "90%",
         }}
       >
-        <Text style={{ fontSize: 20, fontWeight: "600", marginBottom: 20 }}>
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "600",
+            marginBottom: 20,
+            textAlign: "center",
+          }}
+        >
           Add a New Friend
         </Text>
-        <TextInput placeholder="Enter User Id" />
+        <View style={{ borderBottomWidth: 1 }}>
+          <TextInput
+            placeholder="Enter User Id"
+            defaultValue=""
+            onChangeText={(data) => {
+              setFriendId(data);
+            }}
+          />
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            const friendRef = firebase
+              .firestore()
+              .collection("Users")
+              .doc(friendId);
+            addFriend(friendRef).then(() => toggleOverlay());
+          }}
+        >
+          <Text style={{ textAlign: "center", marginTop: 40 }}>Add</Text>
+        </TouchableOpacity>
       </Overlay>
     </View>
   );
